3b7c4a79b5d3348f60fc3bed6855fd7594a43461
João Victor Rocha Santos <rocha.joao.victor.50@gmail.com>
2023-05-15 10:33:37 -0300

    Biblioteca do cookies instalada, validação token feita com sucesso

--- services/users.js
+++ services/users.js
@@ -1,18 +1,41 @@
+import jwt from 'jsonwebtoken';
+
 let users = []
+
+const SECRET = process.env.JWT_SECRET;
+
+function criarToken(user) {
+    return jwt.sign({ email: user.email, nome: user.nome }, SECRET);
+}
+
+function lerToken(token) {
+    try {
+        return jwt.verify(token, SECRET)
+    } catch (error) {
+        throw new Error('Token inválido');
+    }
+}
 
 export function cadastro(body) {
     const user = users.find(({ email }) => email  === body.email);
+    
     if (user) throw new Error('Usuario já cadastrado');
 
     users.push(body);
-    return body
+    
+    const token = criarToken(body);
+
+    return token;
 }
 
 export function login(body) {
     const user = users.find(({ email }) => email  === body.email);
+    
     if (!user) throw new Error('Usuario não encontrado');
+    
     if (user.senha !== body.senha) throw new Error('Senha incorreta');
 
-    return user;
-
+    const token = criarToken(user);
+    
+    return token;
 }
